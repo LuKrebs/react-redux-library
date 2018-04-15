5fd14ead126d9e67a05685039d29bcfdc06c837b
lucianokrebs <lkrebs05@gmail.com>
2018-04-15 16:15:05 -0300

    a simple app using react and redux to become familiar with the core concepts of state management

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,10 @@
+export default function(state = null, action) {
+    switch (action.type) {
+
+    case 'BOOK_SELECTED':
+        console.log(action);
+        return action.payload;
+    }
+    
+    return state;
+}
